6bbb1bc7db8e5b9ad9772603dae139474f4e65d0
murat_can_sarmali <sarmalimuratcan307@gmail.com>
2026-03-21 00:02:17 +0300

    fix flask app

--- backend/app.py
+++ backend/app.py
@@ -33,6 +33,9 @@ def create_app(config_class=Config):
     return app, socketio
 
 
+# Create app and socketio instances for production
+app, socketio = create_app()
+
+
 if __name__ == "__main__":
-    app, socketio = create_app()
     socketio.run(app, debug=True, host="0.0.0.0", port=5000)
