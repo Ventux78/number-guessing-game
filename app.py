69d762b69c2ac7142855255881df9b379ba55337
murat_can_sarmali <sarmalimuratcan307@gmail.com>
2026-03-21 00:29:48 +0300

    fix fix fix

--- app.py
+++ app.py
@@ -39,4 +39,7 @@ app, socketio = create_app()
 
 if __name__ == "__main__":
     port = int(os.getenv('PORT', 5000))
-    socketio.run(app, debug=False, host="0.0.0.0", port=port)
+    # Use eventlet for production
+    import eventlet
+    eventlet.monkey_patch()
+    socketio.run(app, debug=False, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
